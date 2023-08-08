a2ac42258a7728e10407af6c0b7f35217e1aa46c
lifeisfakenews <lifeisfakenews69@gmail.com>
2023-08-08 16:50:29 +0000

    Fixed depenencies and added mdx

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
 import { defineConfig } from 'astro/config';
-//import mdx from '@astrojs/mdx';
+import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://guides.planstogether.net",
   base: "/guides",
-//  integrations: [mdx()],
+  integrations: [mdx()]
 });
